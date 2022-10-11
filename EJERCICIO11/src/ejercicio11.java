3d2e0b6452b4302e09dc71fc61c8454a88b5e452
pablo status <Pablo@Pablo>
2022-10-11 12:39:23 +0200

    [ADD] ejercicio hasta el 14

--- EJERCICIO11/src/ejercicio11.java
+++ EJERCICIO11/src/ejercicio11.java
@@ -2,6 +2,13 @@
 public class ejercicio11 {
 	
 	public static void main(String[] args) {
+		float f =180f;
+		float c = 100f;
+		int r = 32;
+		float formula;
+		int v = 60;
+		formula = (v-r)*c/f;
+		System.out.println (formula) ;
 		
 	}
 
--- EJERCICIO13/src/Ejercicio13.java
+++ EJERCICIO13/src/Ejercicio13.java
@@ -2,7 +2,12 @@
 public class Ejercicio13 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		float diametro =4f;
+		int PI = 31416;
+		float pi;
+		pi= PI / 1000;
+		
+		System.out.println("El área de la circunferencia con diametro " + diametro + " es " + ((diametro*diametro)/4));
 
 	}
 
