0f520c8714308e0f1036f68d753ecd5008cf5ecf
pablo status <Pablo@Pablo>
2022-10-13 11:00:00 +0200

    nuevo1

---
+++ EJERCICIO27/src/Ejercicio27.java
@@ -0,0 +1,20 @@
+
+public class Ejercicio27 {
+	public static void main(String[] arg) {
+		int e = 31;
+		float p = 65f;
+		float a = 160f;
+		float R;
+		float v = 665.1f;
+		float c = 1.8f;
+		float x = 4.7f;
+		float z = 9.6f;
+		float A;
+		A= a/100;
+		R= (v+z*p) +(c*a)-(x*e);
+		
+		System.out.println("El REE del paciente con:" + p +"kgs y" + A + "metros es de" + R + ".");
+	}
+
+}
+
