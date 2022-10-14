faf7f29357fbf6c00489598e1cc6f3577014cf4f
pablo status <Pablo@Pablo>
2022-10-14 11:50:05 +0200

    nuevo

---
+++ TEMA 1/EJERCICIO26/src/Ejercicio26.java
@@ -0,0 +1,20 @@
+
+public class Ejercicio26 {
+
+	public static void main(String[] args) {
+		int a = 182;
+		float p = 82f;
+		double A;
+		double v = 0.32810;
+		double b = 0.33929;
+		double x = 29.5336;
+		double mmc;
+		mmc = ((v*p)+ (b*a)-x);
+		A = a/100;
+		
+		System.out.println("El MMC de la persona con" + p + "kgs y" + A + "metros es de" +mmc + ".");
+		
+
+	}
+
+}
