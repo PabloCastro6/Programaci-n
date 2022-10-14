707abc809abb3978b9504c30f632c4a5913899b6
Pablo <pablocm320@gmail.com>
2022-10-14 18:44:23 +0200

    nuevo

---
+++ PROYECTO24/src/ejercicio24.java
@@ -0,0 +1,13 @@
+
+public class ejercicio24 {
+
+	public static void main(String[] args) {
+		int base = 7;
+		double altura= 4.2;
+		double perimetro;
+		perimetro = base*altura;
+		System.out.println("El poligono regular de lado:" + altura + " y " + base + " numeros de lados tiene un perimetro de " + perimetro+ ".");
+
+	}
+
+}
