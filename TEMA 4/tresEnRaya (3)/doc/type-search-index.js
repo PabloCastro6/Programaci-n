8054c261a58207febee990dae0226701903b51a3
pablo status <Pablo@Pablo>
2023-02-09 10:33:23 +0100

    arreglo proyecto tresenraya

--- TEMA 4/tresEnRaya (3)/doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"tresEnRaya","l":"juego"},{"p":"tresEnRaya","l":"Tablero"},{"p":"tresEnRaya","l":"TresEnRaya"}];updateSearchResults();
