e87ced2d3de4d1b40167d3a6adab8154b1606914
pablo status <Pablo@Pablo>
2023-03-16 11:35:46 +0100

    Practica1

---
+++ TEMA 6/Excepciones/src/Practica1.java
@@ -0,0 +1,21 @@
+
+public class Practica1 {
+	    public static void main(String[] args) {
+	        // Imprimir los argumentos utilizando un bucle for
+	        for (int i = 0; i < args.length; i++) {
+	            System.out.println(args[i]);
+	           
+	            
+	            // Bucle for con un error de índice
+	            int[] array = {1, 2, 3};
+	            
+	            for (int x = 0; x <= array.length; x++) {
+	                System.out.println(array[x]);
+	        }
+	        }
+	    }
+}
+	
+
+
+
