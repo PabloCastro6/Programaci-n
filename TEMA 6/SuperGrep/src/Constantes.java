69488a11e8136d651edaf47d11d7220ccc4ac4dd
pablo status <Pablo@Pablo>
2023-04-13 11:35:35 +0200

    Practica

---
+++ TEMA 6/SuperGrep/src/Constantes.java
@@ -0,0 +1,6 @@
+
+public class Constantes {
+
+	public static final String DIRECTORIO_DESTINO = "C:\\resultado\\";
+	public static final String DIRETORIO_LOG = "C:\\logsSuperGrep\\";
+}
--- TEMA 6/superGrep/src/superGrep/constantes.java
+++
@@ -1,5 +0,0 @@
-package superGrep;
-
-public class constantes {
-
-}
