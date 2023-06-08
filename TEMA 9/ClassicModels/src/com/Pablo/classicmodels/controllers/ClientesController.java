c3032b309fc7c811a8cc016a68b9be822d7e4d1a
pablo status <Pablo@Pablo>
2023-06-08 13:50:43 +0200

    ClassicModels

--- TEMA 9/ClassicModels/src/com/Pablo/classicmodels/controllers/ClientesController.java
+++ TEMA 9/ClassicModels/src/com/Pablo/classicmodels/controllers/ClientesController.java
@@ -8,19 +8,24 @@ import com.Pablo.classicmodels.model.ClientesModelo;
 
 public class ClientesController {
 	
-	 public List<ClienteDTO> recuperarClienteFiltraPorNombre(String nombre,String pais,String telefono) throws ClassNotFoundException, SQLException {
+	 public List<ClienteDTO> recuperarClienteFiltraPorNombre(String nombre) throws ClassNotFoundException, SQLException {
 	 
 	 ClientesModelo cm = new ClientesModelo();
 	 
-	 return cm.recuperaClienteFiltraPorNombre(nombre,pais,telefono);
+	 return cm.recuperarClienteFiltraPorNombre(nombre);
 
 }
+	 public List<ClienteDTO> recuperaNombreTelefono(String nombre, String telefono, String pais) throws ClassNotFoundException, SQLException{
+			ClientesModelo cm = new ClientesModelo();
+			return cm.recuperarClienteFiltraPorNombreTelefono(nombre, telefono, pais);
+		}
+	 
 	 public Integer insertarCliente(Integer customerName,String customerNumber,String contactLastName,String phone,String addresLine1, String addresLine2, 
 				String city, String state, String postalCode,
 				String country, Integer salesRepEmployeeNumber, Double creditLimit  ) throws ClassNotFoundException, SQLException {
 		
 	ClientesModelo cm = new ClientesModelo();
-	return cm.insertarCliente(customerName, customerNumber, contactLastName, phone, addresLine1, addresLine2, city, state, postalCode, country, salesRepEmployeeNumber, creditLimit);
+	return cm.actualizarCliente(customerName, customerNumber, contactLastName, phone, addresLine1, addresLine2, city, state, postalCode, country, salesRepEmployeeNumber, creditLimit);
 } 
 	 
 	 public Integer actualizarCliente (String customerName,String customerNumber,String contactLastName,String phone,String addresLine1, String addresLine2, 
